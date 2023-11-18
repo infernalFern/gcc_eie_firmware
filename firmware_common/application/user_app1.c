8d1a86a7be17677169effa4ff90ba30d43f19494
infernalFern <139037217+infernalFern@users.noreply.github.com>
2023-11-17 20:03:14 -0700

    Update: Module completed

--- firmware_common/application/user_app1.c
+++ firmware_common/application/user_app1.c
@@ -92,6 +92,8 @@ Promises:
 */
 void UserApp1Initialize(void)
 {
+  PWMAudioSetFrequency(BUZZER1, 500);
+  
   /* If good initialization, set state to Idle */
   if( 1 )
   {
@@ -140,7 +142,34 @@ State Machine Function Definitions
 /* What does this state do? */
 static void UserApp1SM_Idle(void)
 {
+  if (WasButtonPressed(BUTTON0)) {
+    ButtonAcknowledge(BUTTON0);
+    PWMAudioSetFrequency(BUZZER1, 262);
+  }
+
+  if (WasButtonPressed(BUTTON1)) {
+    ButtonAcknowledge(BUTTON1);
+    PWMAudioSetFrequency(BUZZER1, 294);
+  }
+
+  if (WasButtonPressed(BUTTON2)) {
+    ButtonAcknowledge(BUTTON2);
+    PWMAudioSetFrequency(BUZZER1, 330);
+  }
+
+  if (WasButtonPressed(BUTTON3)) {
+    ButtonAcknowledge(BUTTON3);
+    PWMAudioSetFrequency(BUZZER1, 392);
+  }
+
+  if (IsButtonPressed(BUTTON0) || IsButtonPressed(BUTTON1) ||
+      IsButtonPressed(BUTTON2) || IsButtonPressed(BUTTON3)) {
     
+    PWMAudioOn(BUZZER1);
+  } else {
+    PWMAudioOff(BUZZER1);
+  }
+  
 } /* end UserApp1SM_Idle() */
      
 
